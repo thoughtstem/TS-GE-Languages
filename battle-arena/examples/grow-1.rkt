1b9be7d8c5c4d05a7806685e9c9162e212353048
Sonny Najar <rodavlasrajan@gmail.com>
2019-01-03 14:37:19 -0800

    katas for camp 1/4

---
+++ battle-arena/examples/grow-1.rkt
@@ -0,0 +1,8 @@
+#lang battle-arena
+
+(define-kata-code battle-arena grow-1
+  
+  (battle-arena-game
+   #:item-list (list (custom-item #:name   "Grow Potion"
+                                  #:sprite (make-icon "BIG" 'red 'white)
+                                  #:on-use (scale-sprite 2 #:for 100)))))
---
+++ battle-arena/examples/grow-and-shrink.rkt
@@ -0,0 +1,19 @@
+#lang battle-arena
+
+(define-kata-code battle-arena grow-and-shrink
+
+  (define (grow-potion)
+    (custom-item #:name   "Grow Potion"
+                 #:sprite (make-icon "BIG" 'red 'white)
+                 #:on-use (scale-sprite 2 #:for 100)
+                 #:rarity 'common))
+
+  (define (shrink-potion)
+    (custom-item #:name     "Shrink Potion"
+                 #:sprite   (make-icon "SML" 'blue 'white)
+                 #:on-use   (scale-sprite 0.5 #:for 100)
+                 #:rarity   'rare))
+  
+  (battle-arena-game
+   #:item-list      (list (grow-potion)
+                          (shrink-potion))))

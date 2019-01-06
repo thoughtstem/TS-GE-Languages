7c269ae86df606e55acfdad79be057e796cf0013
srfoster <stephen@thoughtstem.com>
2019-01-05 16:47:32 -0800

    putting ts-kata-util back as a dep for battle-arena

--- battle-arena/info.rkt
+++ battle-arena/info.rkt
@@ -5,7 +5,7 @@
 
 (define deps '(
   "https://github.com/thoughtstem/game-engine.git"
-  ;"https://github.com/thoughtstem/TS-Kata-Collections.git?path=ts-kata-util"
+  "https://github.com/thoughtstem/TS-Kata-Collections.git?path=ts-kata-util"
   ;"https://github.com/thoughtstem/game-engine-rpg.git"
   "https://github.com/thoughtstem/game-engine-demos.git?path=game-engine-demos-common"
   ))

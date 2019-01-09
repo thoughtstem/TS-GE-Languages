1f94bccfb2b27e2b5a1ea5a5a389ab583bf774e4
aBlender <JasonLongLe@gmail.com>
2019-01-08 18:07:46 -0800

    Removed scoring and references to battle-arena from lang survival

--- survival/jam-lang.rkt
+++ survival/jam-lang.rkt
@@ -2,22 +2,23 @@
 
 
 (provide
- (all-from-out "scoring/score.rkt")
+ ;(all-from-out "scoring/score.rkt")
  (all-from-out "jam/survival-game-jam.rkt")
- (all-from-out "jam/battle-arena-game-jam.rkt")
+ ;(all-from-out "jam/battle-arena-game-jam.rkt")
  (all-from-out "browsable-assets.rkt")
- (all-from-out ts-kata-util)
+ ;(all-from-out ts-kata-util)
  (all-from-out game-engine)
  (all-from-out game-engine-demos-common)
  (all-from-out 2htdp/image)
  (all-from-out racket)
  #%module-begin)
 
-(require "scoring/score.rkt")
+;(require "scoring/score.rkt")
 (require "jam/survival-game-jam.rkt")
-(require "jam/battle-arena-game-jam.rkt")
-(require game-engine)
-(require ts-kata-util)
+;(require "jam/battle-arena-game-jam.rkt")
+(require (except-in game-engine
+                    change-health-by))
+;(require ts-kata-util)
 (require game-engine-demos-common)
 (require 2htdp/image)
 (require "browsable-assets.rkt")

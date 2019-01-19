abc24c8ff779d7fd22bc7c726c3b097458955058
aBlender <JasonLongLe@gmail.com>
2019-01-18 17:50:57 -0800

    Moved Survival Game Example to examples and removed module test

--- survival/jam/survival-game-jam.rkt
+++ survival/jam/survival-game-jam.rkt
@@ -1184,34 +1184,9 @@
         (change-stat "health" e amt)
         (set-stat "health" e max))))
 
-(module+ test
-  (survival-game
-   #:bg     (custom-background #:bg-img FOREST-BG)
-   #:avatar (custom-avatar #:sprite (random-character-sprite))
-   #:starvation-rate 20
-   #:sky        (custom-sky #:length-of-day 500
-                            #:night-sky-color 'darkmagenta
-                            #:max-darkness 150)
-   #:coin-list  (list (custom-coin))
-   #:npc-list   (list (custom-npc))
-   #:enemy-list (list (custom-enemy #:sprite bat-sprite
-                                    #:ai 'medium
-                                    #:amount-in-world 10
-                                    #:night-only? #t)
-                      (custom-enemy #:sprite slime-sprite
-                                    #:ai 'easy
-                                    #:amount-in-world 10)
-                      ; adding curry allows each enemy to have a random sprite
-                      ;(curry custom-enemy #:amount-in-world 10)
-                      )
-   #:food-list (list (custom-food #:name "Carrot"
-                                  #:sprite carrot-sprite
-                                  #:amount-in-world 30)
-                     (custom-food #:name "Carrot Stew"
-                                  #:sprite carrot-stew-sprite
-                                  #:heals-by 40
-                                  #:respawn? #f))
-   #:crafter-list (list (custom-crafter))))
+(module test racket
+  (displayln "TEST!!!")
+  )
 
 
 

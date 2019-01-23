9b9d4839035545d5bab65f2512a967cda9dbd660
srfoster <stephen@thoughtstem.com>
2019-01-23 12:06:31 -0800

    hotfix, remove unnecessary dep

--- survival/info.rkt
+++ survival/info.rkt
@@ -3,7 +3,6 @@
 (define scribblings '(("scribblings/manual.scrbl" (multi-page))))
 
 (define deps '(
-  "https://github.com/thoughtstem/ts-curric-common.git"
   "https://github.com/thoughtstem/TS-Kata-Collections.git?path=ts-kata-util"
   "https://github.com/thoughtstem/game-engine.git"
   "https://github.com/thoughtstem/game-engine-rpg.git"

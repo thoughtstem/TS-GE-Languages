fcc4bea6fdc6ec53357025b5027616ee6c1b8f63
thoughtstem <sonny.najar@gmail.com>
2019-07-19 08:34:05 -0700

    new k2 fall 2019 examples

--- k2/lang/animal/coins.rkt
+++ k2/lang/animal/coins.rkt
@@ -12,7 +12,11 @@
   k2/lang/animal/animal-lang)
 
 (define rand
-    (lambda () (first (shuffle (list cat dog apple mushroom onion potato gold silver copper)))))
+    (lambda () (first (shuffle (list cat dog rabbit
+                                     sheep turkey
+                                     wolf apple mushroom
+                                     onion potato
+                                     gold silver copper)))))
 
 (module ratchet racket 
   (require ratchet
@@ -31,37 +35,42 @@
     (lambda () (first (shuffle (list cat dog apple mushroom onion potato gold silver copper)))))
 
   (define-visual-language #:wrapper launch-for-ratchet
-                          animal-lang
-                          "./animal-lang.rkt"
-                          ;Animals
-                          [cat      c (s:scale-to-fit (s:draw-sprite cat)      32)]
-                          [dog      d (s:scale-to-fit (s:draw-sprite dog)      32)]
-                          ;[horse    h (s:scale-to-fit (s:draw-sprite horse)    32)]
-                          ;[rabbit   r (s:scale-to-fit (s:draw-sprite rabbit)   32)]
-                          ;[turkey   t (s:scale-to-fit (s:draw-sprite turkey)   32)]
-                          ;[wolf     w (s:scale-to-fit (s:draw-sprite wolf)     32)]
+    animal-lang
+    "./animal-lang.rkt"
+    ;Animals
+    [cat      c (s:scale-to-fit (s:draw-sprite cat)      32)]
+    [dog      d (s:scale-to-fit (s:draw-sprite dog)      32)]
+    ;[goat     g (s:scale-to-fit (s:draw-sprite goat)     32)]
+    ;[horse    h (s:scale-to-fit (s:draw-sprite horse)    32)]
+    [rabbit   r (s:scale-to-fit (s:draw-sprite rabbit)   32)]
+    [sheep    s (s:scale-to-fit (s:draw-sprite sheep)    32)]
+    [turkey   t (s:scale-to-fit (s:draw-sprite turkey)   32)]
+    [wolf     w (s:scale-to-fit (s:draw-sprite wolf)     32)]
 
-                          ;Foods
-                          [apple    a (s:scale-to-fit (s:draw-sprite apple)    32)]
-                          [mushroom m (s:scale-to-fit (s:draw-sprite mushroom) 32)]
-                          [onion    o (s:scale-to-fit (s:draw-sprite onion)    32)]
-                          [potato   p (s:scale-to-fit (s:draw-sprite potato)   32)]
+    ;Foods
+    [apple     a (s:scale-to-fit (s:draw-sprite apple)     32)]
+    [broccoli  b (s:scale-to-fit (s:draw-sprite broccoli)  32)]
+    [grapes    g (s:scale-to-fit (s:draw-sprite grapes)    32)]
+    [kiwi      k (s:scale-to-fit (s:draw-sprite kiwi)      32)]
+    [mushroom  m (s:scale-to-fit (s:draw-sprite mushroom)  32)]
+    [onion     o (s:scale-to-fit (s:draw-sprite onion)     32)]
+    [pineapple p (s:scale-to-fit (s:draw-sprite pineapple) 32)]
 
-                          ;Coins
-                          [copper   x (s:scale-to-fit (s:draw-sprite copper)   32)]
-                          [silver   y (s:scale-to-fit (s:draw-sprite silver)   32)]
-                          [gold     z (s:scale-to-fit (s:draw-sprite gold)     32)]
+    ;Coins
+    [copper   x (s:scale-to-fit (s:draw-sprite copper)   32)]
+    [silver   y (s:scale-to-fit (s:draw-sprite silver)   32)]
+    [gold     z (s:scale-to-fit (s:draw-sprite gold)     32)]
                           
-                          ;Colors
-                          [red            R (h:square 32 'solid 'red)]
-                          [orange         O (h:square 32 'solid 'orange)]
-                          [yellow         Y (h:square 32 'solid 'yellow)]
-                          [green          G (h:square 32 'solid 'green)]
-                          [blue           B (h:square 32 'solid 'blue)]
-                          [purple         P (h:square 32 'solid 'purple)]
+    ;Colors
+    [red            R (h:square 32 'solid 'red)]
+    [orange         O (h:square 32 'solid 'orange)]
+    [yellow         Y (h:square 32 'solid 'yellow)]
+    [green          G (h:square 32 'solid 'green)]
+    [blue           B (h:square 32 'solid 'blue)]
+    [purple         P (h:square 32 'solid 'purple)]
     
-                          ;Other
-                          [rand     ? question-icon]
-                          [start    = play-icon]))
+    ;Other
+    [rand     ? question-icon]
+    [start    = play-icon]))
 
 
--- k2/lang/animal/enemies.rkt
+++ k2/lang/animal/enemies.rkt
@@ -28,40 +28,50 @@
     (h:crop 0 0 0 32 32 i))
 
   (define rand
-    (lambda () (first (shuffle (list cat dog horse rabbit turkey wolf apple mushroom onion potato gold silver copper)))))
+    (lambda () (first (shuffle (list cat dog goat
+                                     horse rabbit
+                                     sheep turkey
+                                     wolf apple mushroom
+                                     onion potato
+                                     gold silver copper)))))
 
   (define-visual-language #:wrapper launch-for-ratchet
-                          animal-lang
-                          "./animal-lang.rkt"
-                          ;Animals
-                          [cat      c (s:scale-to-fit (s:draw-sprite cat)      32)]
-                          [dog      d (s:scale-to-fit (s:draw-sprite dog)      32)]
-                          [horse    h (s:scale-to-fit (s:draw-sprite horse)    32)]
-                          [rabbit   r (s:scale-to-fit (s:draw-sprite rabbit)   32)]
-                          [turkey   t (s:scale-to-fit (s:draw-sprite turkey)   32)]
-                          [wolf     w (s:scale-to-fit (s:draw-sprite wolf)     32)]
-
-                          ;Foods
-                          [apple    a (s:scale-to-fit (s:draw-sprite apple)    32)]
-                          [mushroom m (s:scale-to-fit (s:draw-sprite mushroom) 32)]
-                          [onion    o (s:scale-to-fit (s:draw-sprite onion)    32)]
-                          [potato   p (s:scale-to-fit (s:draw-sprite potato)   32)]
-
-                          ;Coins
-                          [copper   x (s:scale-to-fit (s:draw-sprite copper)   32)]
-                          [silver   y (s:scale-to-fit (s:draw-sprite silver)   32)]
-                          [gold     z (s:scale-to-fit (s:draw-sprite gold)     32)]
-                          
-                          ;Colors
-                          [red            R (h:square 32 'solid 'red)]
-                          [orange         O (h:square 32 'solid 'orange)]
-                          [yellow         Y (h:square 32 'solid 'yellow)]
-                          [green          G (h:square 32 'solid 'green)]
-                          [blue           B (h:square 32 'solid 'blue)]
-                          [purple         P (h:square 32 'solid 'purple)]
+    animal-lang
+    "./animal-lang.rkt"
+    ;Animals
+    [cat      c (s:scale-to-fit (s:draw-sprite cat)      32)]
+    [dog      d (s:scale-to-fit (s:draw-sprite dog)      32)]
+    [goat     g (s:scale-to-fit (s:draw-sprite goat)     32)]
+    [horse    h (s:scale-to-fit (s:draw-sprite horse)    32)]
+    [rabbit   r (s:scale-to-fit (s:draw-sprite rabbit)   32)]
+    [sheep    s (s:scale-to-fit (s:draw-sprite sheep)    32)]
+    [turkey   t (s:scale-to-fit (s:draw-sprite turkey)   32)]
+    [wolf     w (s:scale-to-fit (s:draw-sprite wolf)     32)]
     
-                          ;Other
-                          [rand     ? question-icon]
-                          [start    = play-icon]))
+    ;Foods
+    [apple    a (s:scale-to-fit (s:draw-sprite apple)    32)]
+    [banana   b (s:scale-to-fit (s:draw-sprite banana)   32)]
+    [eggplant e (s:scale-to-fit (s:draw-sprite eggplant) 32)]
+    [kiwi     k (s:scale-to-fit (s:draw-sprite kiwi)     32)]
+    [mushroom m (s:scale-to-fit (s:draw-sprite mushroom) 32)]
+    [onion    o (s:scale-to-fit (s:draw-sprite onion)    32)]
+    [pepper   p (s:scale-to-fit (s:draw-sprite pepper)   32)]
+
+    ;Coins
+    [copper   x (s:scale-to-fit (s:draw-sprite copper)   32)]
+    [silver   y (s:scale-to-fit (s:draw-sprite silver)   32)]
+    [gold     z (s:scale-to-fit (s:draw-sprite gold)     32)]
+                          
+    ;Colors
+    [red            R (h:square 32 'solid 'red)]
+    [orange         O (h:square 32 'solid 'orange)]
+    [yellow         Y (h:square 32 'solid 'yellow)]
+    [green          G (h:square 32 'solid 'green)]
+    [blue           B (h:square 32 'solid 'blue)]
+    [purple         P (h:square 32 'solid 'purple)]
+    
+    ;Other
+    [rand     ? question-icon]
+    [start    = play-icon]))
 
 

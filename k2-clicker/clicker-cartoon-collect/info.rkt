5c744a4687587c7c91dbba996e73209ad77fa588
srfoster <stephen@thoughtstem.com>
2019-10-02 15:22:18 -0700

    fixing deps

--- k2-clicker/clicker-cartoon-collect/info.rkt
+++ k2-clicker/clicker-cartoon-collect/info.rkt
@@ -1,6 +1,10 @@
 #lang info
 (define collection "clicker-farm-collect")
-(define deps '("base"))
+(define deps '("base" 
+               "cartoon-assets" 
+               "clicker-lib"
+               "adventure"
+               ))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/clicker-farm-collect.scrbl" ())))
 (define pkg-desc "Description Here")
--- k2-healer/info.rkt
+++ k2-healer/info.rkt
@@ -1,3 +1,4 @@
 #lang info
 (define collection 'multi)
 (define pkg-authors '(thoughtstem))
+(define deps '("survival"))
